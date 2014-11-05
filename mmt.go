7fa5ba041435dd851e05f33399a005d169d6d9db
it0a <recursive404@gmail.com>
2014-11-04 23:11:58 -0500

    Adding updates to mmt to include proper json parsing

--- mmt.go
+++ mmt.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/codegangsta/cli"
 	"io/ioutil"
 	"os"
@@ -11,20 +10,29 @@ import (
 )
 
 type DbConfig struct {
-	host   string
-	port   string
-	user   string
-	pass   string
-	schema string
+	Host   string `json:"host"`
+	Port   string `json:"port"`
+	User   string `json:"user"`
+	Schema string `json:"schema"`
 }
 
 type Table struct {
-	name string
+	Name string `json:"name"`
+}
+
+type TableProfile struct {
+	Name   string  `json:"name"`
+	Tables []Table `json:"tables"`
+}
+
+type DbProfile struct {
+	Name     string   `json:"name"`
+	DbConfig DbConfig `json:"dbConfig"`
 }
 
 type Config struct {
-	dbconfig DbConfig
-	tables   []Table
+	DbProfiles    []DbProfile    `json:"dbProfiles"`
+	TableProfiles []TableProfile `json:"tableProfiles"`
 }
 
 func main() {
@@ -64,21 +72,38 @@ func main() {
 }
 
 func do_info() {
-	config := ReadConfig()
-	println("Host: " + config.dbconfig.host)
+	Config := ReadConfig()
+	for _, dbProfile := range Config.DbProfiles {
+		println("Database Profile Name: " + dbProfile.Name)
+		println("Host: " + dbProfile.DbConfig.Host)
+		println("Port: " + dbProfile.DbConfig.Port)
+		println("User: " + dbProfile.DbConfig.User)
+		println("Schema: " + dbProfile.DbConfig.Schema)
+		println("")
+	}
+	for _, tableProfile := range Config.TableProfiles {
+		println("Table Profile Name: " + tableProfile.Name)
+		println("Tables: ")
+		for _, table := range tableProfile.Tables {
+			println(table.Name)
+		}
+		println("")
+	}
+
 }
 
 func ReadConfig() Config {
-	file, readErr := ioutil.ReadFile("mmt.config")
+	file, readErr := ioutil.ReadFile("./mmt.json")
 	if readErr != nil {
 		panic(readErr)
+		os.Exit(1)
 	}
 	var config Config
-	parseErr := json.Unmarshal([]byte(file), &config)
+	parseErr := json.Unmarshal(file, &config)
 	if parseErr != nil {
 		panic(parseErr)
+		os.Exit(1)
 	}
-	fmt.Printf("%#v\n", config)
 	return config
 }
 
@@ -97,6 +122,7 @@ func do_dump() {
 	execErr := syscall.Exec(get_binary(), args, env)
 	if execErr != nil {
 		panic(execErr)
+		os.Exit(1)
 	}
 }
 
